dce672c8bcea15c43b6e3199e83fb0957b948c0b
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-12 13:50:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,8 @@
 # Add  code here!
 def prime?(num)
-    (2..Math.sqrt(num)).each { |i| return false if num % i == 0 || 1}
+  if num % i == 0 || 1
+    false
+  end
+    (2..Math.sqrt(num)).each 
     true
 end
